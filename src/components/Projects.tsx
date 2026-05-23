2c769dc2a19b596d61de1ee7150b80c8673f4e8c
GalalElaasar9 <138381852+GalalElaasar9@users.noreply.github.com>
2026-05-23 04:02:41 +0300

    ...

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -55,63 +55,65 @@ export default function Projects({ project }: Props) {
       <div className="absolute inset-0 bg-black/10 group-hover:bg-black/0 transition-colors duration-500" />
 
       {/* Sub Images */}
-      <div
-        className="
-          absolute bottom-2 left-[50%] translate-x-[-50%]
-          flex items-center
-          overflow-x-auto scrollbar-hide
-          
-          bg-background/40
-          backdrop-blur-md
-          border border-white/10
-          w-fit
-          lg:w-auto
-          rounded-2xl
-          px-2 py-2
-          md:w-fit md:right-auto
+      {subImages && subImages.length > 0 && (
+        <div
+          className="
+            absolute bottom-2 left-[50%] translate-x-[-50%]
+            flex items-center
+            overflow-x-auto scrollbar-hide
+            
+            bg-background/40
+            backdrop-blur-md
+            border border-white/10
+            w-fit
+            lg:w-auto
+            rounded-2xl
+            px-2 py-2
+            md:w-fit md:right-auto
 
-          opacity-100 translate-y-0
-          md:opacity-0 md:translate-y-6
-          md:group-hover:translate-y-0
-          md:group-hover:opacity-100
+            opacity-100 translate-y-0
+            md:opacity-0 md:translate-y-6
+            md:group-hover:translate-y-0
+            md:group-hover:opacity-100
 
-          transition-all duration-500
-        "
-      >
-        {subImages?.map((img, index) => (
-          <div
-            key={img.id}
-            onClick={()=>handleSelectImage(img.subImage)}
-            className={`              
-              shrink-0
-              w-11 h-11
-              sm:w-16 sm:h-16
-              md:w-18 md:h-18
-              lg:w-20 lg:h-20
+            transition-all duration-500
+          "
+        >
+          {subImages?.map((img, index) => (
+            <div
+              key={img.id}
+              onClick={()=>handleSelectImage(img.subImage)}
+              className={`              
+                shrink-0
+                w-11 h-11
+                sm:w-16 sm:h-16
+                md:w-18 md:h-18
+                lg:w-20 lg:h-20
 
-              rounded-xl
-              overflow-hidden
-              border-2 border-background
-              cursor-pointer
-              shadow-lg
+                rounded-xl
+                overflow-hidden
+                border-2 border-background
+                cursor-pointer
+                shadow-lg
 
-              transition-all duration-300
-              hover:-translate-y-2 hover:scale-105   
-              `}
+                transition-all duration-300
+                hover:-translate-y-2 hover:scale-105   
+                `}
 
-            style={{
-              marginLeft: index === 0 ? 0 : 0,
-              zIndex: subImages.length - index,
-            }}
-          >
-            <img
-              src={img.subImage}
-              alt={img.subImage}
-              className="w-full h-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
+              style={{
+                marginLeft: index === 0 ? 0 : 0,
+                zIndex: subImages.length - index,
+              }}
+            >
+              <img
+                src={img.subImage}
+                alt={img.subImage}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 }
